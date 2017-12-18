1a1cd21954c245d41041d651091a8de82979ffab
Trọng Hữu <nhan2768@gmail.com>
2017-12-18 21:41:43 +0700

    First commit

--- src/app/entities/login/login.service.ts
+++ src/app/entities/login/login.service.ts
@@ -1,28 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { Http, Response, ResponseOptions, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/bindCallback';
 import { environment } from '../../../environments/environment';
+
 @Injectable()
 export class LoginService {
     private urlLogin = '/api/auth/login';
     private urlProfile = '/api/user/profile';
     constructor(private http: Http) { }
-    private getHeaders(): RequestOptions {
-        let reqOp = new RequestOptions();
-    
-        let headers = new Headers();
-        headers.append('Accept', 'application/json');
-        headers.append('Access-Control-Allow-Origin', 'https://foodsyapp.herokuapp.com');
-        headers.append('Access-Control-Allow-Headers', '*');
-    
-        reqOp.headers = headers;
-    
-        return reqOp;
-      }
+
     getLogin(user): Observable<any> {
-        return this.http.post(environment.rootUrl + this.urlLogin, user, this.getHeaders()).map((res: Response) => {
+        return this.http.post(environment.rootUrl + this.urlLogin, user).map((res: Response) => {
             const data = res.json().data;
             return data.token;
         }).catch((res: Response) => {
